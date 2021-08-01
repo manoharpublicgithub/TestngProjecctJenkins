63d9177738bf2e0faa2f258c92e06164c4388a19
manohar <manoharreddy.qtp@gmail.com>
2021-08-01 10:18:40 +0530

    first commit

---
+++ src/test/java/Jenkins1Grp/Jenkins1/Program1.java
@@ -0,0 +1,30 @@
+package Jenkins1Grp.Jenkins1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+
+
+//import org.junit.Test;
+
+public class Program1 {
+@Test
+public void test1(){
+	System.out.println("Test1");
+	Assert.assertEquals(true, true);
+}
+
+@Test
+public void test2(){
+	System.out.println("Test2");
+	Assert.assertEquals(true, true);
+}
+
+@Test
+public void test3(){
+	System.out.println("Test3");
+	Assert.assertEquals(true, true);
+}
+
+}
